62d370a1c41fe7248eff4ac92f6a9cf0ecb54d06
zeritte <zeritte@gmail.com>
2019-12-20 01:21:48 +0300

    latest adds

--- src/api_url.js
+++ src/api_url.js
@@ -1,7 +1,10 @@
-export default 'http://localhost:3000/api/'
+const server_url = 'http://localhost:3000'
+// you should change server url with the address you have pushed ruby on rails app.
 
-// tctype, impact and domains can be edited or added from api_url. for example:
-// the api url above is http://localhost:3000/api/, then
+export default `${server_url}/api/`
+
+// tctype, impact and domains can be edited or added from server_url
+// the server url above is http://localhost:3000, then
 // http://localhost:3000/domains/
 // http://localhost:3000/impacts/
 // http://localhost:3000/tctypes/
